5de4a82aee93efbdd00014e2bb4f63dd9d161861
pack0shades <kumarpragay@gmail.com>
2024-10-08 18:51:49 +0530

    raid server setup

--- T5d2.sh
+++ T5d2.sh
@@ -24,11 +24,11 @@ python -m torch.distributed.launch \
         --data-path /scratch/b23es1024/l2p-pytorch/local_datasets/ \
         --output_dir ./output \
         --epochs 8 \
-        --size 20 \
+        --size 25 \
         --embedding_key 'mean_max' \
-        --top_k 6 \
+        --top_k 8 \
         --prompt_key_init 'normal' \
         --prompt_init 'normal' \
         --shared_prompt_pool True \
-        --shared_prompt_key False \
+        --shared_prompt_key True \
         --head_type 'prompt'
