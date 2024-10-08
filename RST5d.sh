4a62e92b37f5df8f876b6e2e0900b152d7b60b57
pack0shades <kumarpragay@gmail.com>
2024-10-09 02:17:10 +0530

    updates

--- RST5d.sh
+++ RST5d.sh
@@ -14,13 +14,14 @@ python -m torch.distributed.launch \
           five_datasets_l2p \
         --model vit_base_patch16_224 \
         --batch-size 16 \
+        --data-path /scratch/b23es1024/l2p-pytorch/local_datasets/ \
         --output_dir ./output \
         --epochs 8 \
-        --size 10 \
+        --size 25 \
         --embedding_key 'mean_max' \
         --top_k 8 \
         --prompt_key_init 'normal' \
         --prompt_init 'normal' \
-        --shared_prompt_pool False \
-        --shared_prompt_key False \
+        --shared_prompt_pool True \
+        --shared_prompt_key True \
         --head_type 'prompt'
